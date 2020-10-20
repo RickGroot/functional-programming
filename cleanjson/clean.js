138e019b62ad636809032c1d1ab870e8206083bc
Rick Groot <RickGroot@gmail.com>
2020-10-20 16:07:33 +0200

    added notes

--- cleanjson/clean.js
+++ cleanjson/clean.js
@@ -1,23 +1,21 @@
-const data = require('./dataviz.json');     //Import de json file zodat ik er hier dingen mee kan doen
+const data = require('./dataviz.json');     //Imports json file
 
-const json = JSON.stringify(data);          //Zet json file om in strings
+const json = JSON.stringify(data);          //Sets everything in json file to strings
 
-const coffee = JSON.parse(json).filter(function (entry) {       //functie die alle personen filtert die het liefst koffie drinken
+const coffee = JSON.parse(json).filter(function (entry) {       //function that filters everyone who drinks coffee
     return entry.theeOfKoffie === 'Koffie';
 })
 
-const rijbewijs = JSON.parse(json).filter(function (entry) {
+const rijbewijs = JSON.parse(json).filter(function (entry) {    //filters everyone without drivers licence
     return entry.rijbewijs.startsWith('N');
 })
 
-const schoenen = JSON.parse(json).filter(function (entry) {
+const schoenen = JSON.parse(json).filter(function (entry) {     //filters people with 4 pairs of shoes and no drivers licence
     if (entry.hoeveelPaarSchoenen.startsWith('4') && entry.rijbewijs.endsWith('ee')) {
         return entry;
     }})
 
 
-console.log('er hebben ' + rijbewijs.length + ' mensen geen rijbewijs');
+console.log('er hebben ' + rijbewijs.length + ' mensen geen rijbewijs');    //puts results in the console
 
-console.log('en ' + schoenen.length + ' van die mensen hebben 4 schoenen');
-
-// console.log("hi")
+console.log('en ' + schoenen.length + ' van die mensen hebben 4 schoenen'); //puts results in the console
