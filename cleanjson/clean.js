f06c73a5b5aca6e73c08ab880caa05b71e0179a7
Rick Groot <RickGroot@gmail.com>
2020-10-22 16:49:35 +0200

    dataviz hobby cleanup session two

--- cleanjson/clean.js
+++ cleanjson/clean.js
@@ -1,5 +1,8 @@
+
 const data = require('./dataviz.json');     //Imports json file
 
+
+// filtering data
 const filterData = JSON.stringify(data);          //Sets everything in json file to strings
 
 const coffee = JSON.parse(filterData).filter(function (entry) {       //function that filters everyone who drinks coffee
@@ -16,6 +19,8 @@ const schoenen = JSON.parse(filterData).filter(function (entry) {     //filters 
     }
 })
 
+console.log(coffee.length + ' mensen drinken alleen koffie')                //puts filtered results in the console
+
 console.log('er hebben ' + rijbewijs.length + ' mensen geen rijbewijs');    //puts filtered results in the console
 
 console.log('en ' + schoenen.length + ' van die mensen hebben 4 schoenen'); //puts filtered results in the console
@@ -26,30 +31,50 @@ console.log('en ' + schoenen.length + ' van die mensen hebben 4 schoenen'); //pu
 
 let column = "hobbies";        //put in column name from json file
 
+
+/* ----------------------------------------------------------simple exersize to get data of one question
 const specific = data.map(element => element[column]);      //creates array with all values of specified column
-
-//console.log(specific);      //logs specific array
-
-
-//same as previous code, but in a function
+console.log(specific);      //logs specific array
+--------------------------------------------------------------*/
 
 let allAnswers = getAnswers(data, column); //calls function with datasheet and column variable
 
 function getAnswers(answer, question) { //function that returns an array of answers of requested question
-    return data.map(answer => answer[question])     //returns array via map
-}
+    return data.map(answer => answer[question]);     //returns array via map
+};
 
-console.log(allAnswers);      //logs answer
 
-// let hobbieArray = [];
 let allHobbies = getHobbies(allAnswers);    //calls function that puts all hobbies into one array
 
 function getHobbies(answers) {
-    return answers.forEach(element => {
-        let arr = element.toLowerCase().replace(/\./gi, ',').split(',');
-        
-        console.log(arr)
-    })
-}
+    let arr = [];                       //creates an array witch wil hold all items
 
-console.log(allHobbies)
+    answers.forEach(element => {        //loops over the big array of answers
+        element                         //selects array that got passed through
+        .toLowerCase()                  //puts everything to lowercase
+        .replace(/\./gi, ',')           //replaces all dots with commas
+        .split(',')                     //splits values in arrays where there is a comma
+        .forEach(childElement =>        //runs through every hobby in an answer
+            arr.push(childElement)      //pushes each individual hobby to array arr
+        );
+    });
+
+    for (i = 0; i < arr.length; i++) {      //for loop that goes through hobby array
+
+        if (arr[i].startsWith(' ')) {       //if the string starts with a space
+            arr[i] = arr[i].substring(1);   //replaces the string from 2nd character, so without the space
+        };
+
+        if (arr[i].startsWith('  ')) {      //if the string starts with 2 spaces
+            arr[i] = arr[i].substring(2);   //replaces the string from 3nd character, so without spaces
+        }; 
+
+        if (arr[i].length < 1) {            //if the string is shorter then 1 character
+            arr[i] = arr.pop(arr[i])        //pops the item out of the array
+        };
+    };
+
+    return arr.sort();      //sorts data alphabetically
+};
+
+console.log(allHobbies);    //logs clean data
